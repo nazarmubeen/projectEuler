434fed6489fa0433391e77ab64dd2cd2709388f3
mnmubeen <fam.nazar@gmail.com>
2017-08-22 15:47:11 +0530

    Problem 22

--- src/level1/Problem22.java
+++ src/level1/Problem22.java
@@ -1,18 +1,14 @@
 package level1;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Problem21 {
+public class Problem22 {
 
 	public static void main(String[] args) throws IOException
 	{
